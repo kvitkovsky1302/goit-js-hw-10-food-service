32367feed42dd6b3ef43d99a17659eddea535dcf
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-06-13 12:32:09 +0300

    update

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -2276,7 +2276,7 @@ var templateFunction = _handlebars.default.template({
           "column": 0
         },
         "end": {
-          "line": 36,
+          "line": 30,
           "column": 9
         }
       }
@@ -2512,7 +2512,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50413" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64427" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import murkupMenu from './js/menu.json';
 import './sass/main.scss';
 import { onChangeTheme, savedTheme, refs } from './js/theme';
 
-
 refs.menuList.insertAdjacentHTML('afterbegin', menuTemplate(murkupMenu));
 refs.themeSwitcher.addEventListener('change', onChangeTheme);
 
